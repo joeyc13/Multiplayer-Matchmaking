2456c81cbd2ff21734f53258087af9deeb4c012b
madgolfer13 <43625757+madgolfer13@users.noreply.github.com>
2022-09-13 02:07:48 +0300

    Create repository

---
+++ matchmaking_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+kills_per_game = np.random.randint(low=0, high=10, size=1000)
+knocks_per_game = np.random.randint(low=0, high=15, size=1000)
+damage_per_game = np.random.randint(low=0, high=4000, size=1000)
+kd_ratio = np.random.randint(low=-4, high=4, size=1000)
+
+# print(kills_per_game[0:10])
+
+total_games
+kills_total = 
+deaths_total
+damage_total
